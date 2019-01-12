ba7a8f2ddd0a99bc96e997673eae3dedddc454d4
Prashanth Rao <pbrao@pmbrao.com>
2019-01-11 23:14:23 -0600

    Provide support for updating items

--- serverless-rest-api/todos/create.js
+++ serverless-rest-api/todos/create.js
@@ -9,7 +9,7 @@ module.exports.create = (event, context, callback) => {
     const timestamp = new Date().getTime();
     const data = JSON.parse(event.body);
     
-    if (typeof data.text !== 'string'){
+    if (typeof data.myText !== 'string'){
         console.error('Validation Failed');
         callback(new Error('Couldn\'t create the todo item.'));
         return;
@@ -18,7 +18,7 @@ module.exports.create = (event, context, callback) => {
         TableName: 'todos',
         Item: {
             id: uuid.v1(),
-            text: data.text,
+            myText: data.myText,
             checked: false,
             createdAt: timestamp,
             updatedAt: timestamp
--- serverless-rest-api/todos/update.js
+++ serverless-rest-api/todos/update.js
@@ -8,33 +8,36 @@ module.exports.update = (event, context, callback) => {
     const timestamp = new Date().getTime();
     const data = JSON.parse(event.body);
 
-    if (typeof data.text !== 'string' || typeof data.checked !== 'boolean' ){
+    if (typeof data.myText !== 'string' || typeof data.checked !== 'boolean' ){
         console.error('Validation error');
-        callback(new Error('Could not update the item'));
+        callback(new Error('Validation failed. Could not update the item'));
         return;
     }
     const params = {
+        Key: {
+            "id": event.pathParameters.id
+        },
         TableName: "todos",
-        Item: {
-            id: event.pathParameters.id,
-            text: data.text,
-            checked: data.checked,
-            updatedAt: timestamp 
+        UpdateExpression: 'SET myText = :newMyText, checked = :newChecked, updatedAt = :newTimestamp',
+        ExpressionAttributeValues: {
+            ':newMyText': data.myText,
+            ':newChecked': data.checked,
+            ':newTimestamp': timestamp 
         }
     }
 
-    dynamoDb.put(params, (error, result) =>{
+    dynamoDb.update(params, (error, result) =>{
         if (error){
             console.error(error);
             callback(new Error('Could not update the item'));
             return;
         }
 
-    const response = {
-        statusCode: 200,
-        body: JSON.stringify(result.Item)
-    }
+        const response = {
+            statusCode: 200,
+            body: JSON.stringify(result.Item)
+        }
 
         callback(null, response);
     })
-    }
+}
